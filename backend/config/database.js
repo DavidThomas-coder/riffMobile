a6f51c420762285b44fc3974b849edae2a566c20
RipNasty <det625@gmail.com>
2025-01-02 21:22:33 -0500

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
               client: 'postgres',
               host: env('DATABASE_HOST', '127.0.0.1'),
               port: env.int('DATABASE_PORT', 5432),
-              database: env('DATABASE_NAME', 'riffMobile_development'),
+              database: env('DATABASE_NAME', 'riffmobile_development'),
               username: env('DATABASE_USERNAME', 'riff_user'),
               password: env('DATABASE_PASSWORD', 'assass1'),
               ssl: env.bool('DATABASE_SSL', false),
